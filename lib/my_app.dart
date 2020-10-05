7cfa9214c1ba5d80aa657825bae618083e1b8654
NguyenTienAnh96 <anhnt@cnv.vn>
2020-10-05 11:15:18 +0700

    task 1

---
+++ lib/my_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_training/src/login_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+}
